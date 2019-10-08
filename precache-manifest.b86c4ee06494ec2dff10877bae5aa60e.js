f13e86f2aff92f02117b33ea3b0c55b32cab50f9
DaringZ <zat938@gmail.com>
2019-10-08 15:05:56 +0500

    Updates

---
+++ precache-manifest.b86c4ee06494ec2dff10877bae5aa60e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddf9180072b15611254271cce596739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82ef25687847d9c8518d",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "966b47661aadb756d65c",
+    "url": "/static/css/main.fffb3267.chunk.css"
+  },
+  {
+    "revision": "82ef25687847d9c8518d",
+    "url": "/static/js/2.c6d875a6.chunk.js"
+  },
+  {
+    "revision": "966b47661aadb756d65c",
+    "url": "/static/js/main.34b3cf31.chunk.js"
+  },
+  {
+    "revision": "25293dcdd045d2663b65",
+    "url": "/static/js/runtime-main.67a84473.js"
+  },
+  {
+    "revision": "fb3fb94c658571bd732ef9a28e60a382",
+    "url": "/static/media/des_portfolio.fb3fb94c.JPG"
+  }
+]);
